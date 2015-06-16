11b42d02ad77c6e10247221564b6cd47bda0b496
Warren Frame <ramblingcookiemonster@users.noreply.github.com>
2015-06-15 20:02:54 -0400

    repo and fork functions
    also, fix old function name in stashproject.

--- PSStash/Get-StashRepo.ps1
+++ PSStash/Get-StashRepo.ps1
@@ -1,24 +1,27 @@
-﻿Function Get-StashProject {
+﻿Function Get-StashRepo {
     <#
     .SYNOPSIS
-        Get Stash Projects
+        Get Stash repositories
     
     .DESCRIPTION
-        Get Stash Projects
-
-    .PARAMETER Key
-        Search for project by Key
+        Get Stash repositories
 
     .PARAMETER Name
-        Search for project by Name
+        Search for repository by name
+
+    .PARAMETER ProjectName
+        Search for repositories under this project
 
     .PARAMETER Permission
-        Search for project by your permission to it.
+        Search for repositories by your permission to it.
 
         Valid permissions:
-            PROJECT_ADMIN
-            PROJECT_WRITE
-            PROJECT_READ
+            REPO_ADMIN
+            REPO_WRITE
+            REPO_READ
+
+    .PARAMETER Visibility
+        Search for repositories by whether they have public or private visibility.
 
     .PARAMETER Uri
         The base Uri for Stash.  Defaults to $StashConfig.Uri
@@ -32,21 +35,28 @@
         A valid PSCredential
 
     .EXAMPLE
-        Get-StashProject
+        Get-StashRepo
 
-        #List public projects on Stash, using the URI from Get-StashConfig/Set-StashConfig
+        #List public repositories on Stash, using the URI from Get-StashConfig/Set-StashConfig
 
     .EXAMPLE
-        Get-StashProject -Key SYSINV -Credential $Cred 
+        Get-StashRepo -Project SystemsInventory -Credential $Cred
 
-        # Get Stash project with key SYSINV
+        # Get Stash repositories under the SystemsInventory project
         # Authenticates with $Cred
         # Uses the URI from Get-StashConfig/Set-StashConfig
 
     .EXAMPLE
-        Get-StashProject -Permission PROJECT_ADMIN -Credential $Cred
+        Get-StashRepo -Name Inventory -Credential $Cred 
 
-        # Get Stash projects where $Cred has PROJECT_ADMIN permissions
+        # Get Stash repository with name Inventory
+        # Authenticates with $Cred
+        # Uses the URI from Get-StashConfig/Set-StashConfig
+
+    .EXAMPLE
+        Get-StashRepo -Permission REPO_ADMIN -Credential $Cred
+
+        # Get Stash repos where $Cred has REPO_ADMIN permissions
         # Uses the URI from Get-StashConfig/Set-StashConfig
 
 
@@ -58,10 +68,13 @@
         [parameter(ParameterSetName = 'Name')]
         [string]$Name,
         [parameter(ParameterSetName = 'Name')]
-        [validateset('PROJECT_READ','PROJECT_WRITE','PROJECT_ADMIN')]
+        [string]$ProjectName,
+        [parameter(ParameterSetName = 'Name')]
+        [validateset('REPO_READ','REPO_WRITE','REPO_ADMIN')]
         [string]$Permission,
-        [parameter(ParameterSetName = 'Key')]
-        [string]$Key,
+        [parameter(ParameterSetName = 'Name')]
+        [validateset('public','private')]
+        [string]$Visibility,
         [string]$Uri = $Script:StashConfig.Uri,
         [System.Management.Automation.PSCredential]$Credential,
         [switch]$Raw
@@ -69,7 +82,7 @@
 
     #Build up URI
         $StashBaseURI = Join-Parts -Separator "/" -Parts $Uri, "/rest/api/1.0"
-        $BaseUri = Join-Parts -Separator "/" -Parts $StashBaseURI, 'projects', $key
+        $BaseUri = Join-Parts -Separator "/" -Parts $StashBaseURI, 'repos'
 
     #Build up Invoke-RestMethod and Get-StashData parameters for splatting
         $IRMParams = @{
@@ -89,9 +102,17 @@
             {
                 $Body.Add( 'name', $Name)
             }
+            'ProjectName'
+            {
+                $Body.Add( 'projectname', $ProjectName)
+            }
             'Permission'
             {
                 $Body.Add( 'permission', $Permission)
+            }
+            'Visibility'
+            {
+                $Body.Add( 'visibility', $Visibility)
             }
         }
         if($Body.keys.count -gt 0)
---
+++ PSStash/New-StashFork.ps1
@@ -0,0 +1,112 @@
+﻿Function New-StashFork {
+    <#
+    .SYNOPSIS
+        Fork a Stash repository
+    
+    .DESCRIPTION
+        Fork a Stash repository
+
+    .PARAMETER Project
+        Project key containing the repository you want to fork
+        
+    .PARAMETER Repo
+        Repository you want to fork
+        
+    .PARAMETER DestinationProject
+        Project to create the fork within.
+        
+        Defaults to your personal 'project'.
+    
+    .PARAMETER DestinationName
+        Fork name.
+        
+        Defaults to the name of the origin repository
+
+    .PARAMETER Uri
+        The base Uri for Stash.  Defaults to $StashConfig.Uri
+        
+        Example: "https://Stash.contoso.com:8443"
+
+    .PARAMETER Credential
+        A valid PSCredential
+
+    .EXAMPLE
+        New-StashFork -Credential $cred -Project SYSINV -Repo SystemsInventory 
+
+        # Forks the SYSINV project's SystemsInventory repository into your personal project
+        # Maintains the 'SystemInventory' repository name
+
+    .EXAMPLE
+        New-StashFork -Credential $cred -Project SYSINV -Repo SystemsInventory -DestinationProject RND
+        
+        # Forks the SYSINV project's SystemsInventory repository into the project with key RND
+        # Maintains the 'SystemInventory' repository name
+
+    .EXAMPLE
+        New-StashFork -Credential $cred -Project SYSINV -Repo SystemsInventory -DestinationProject FRK -DestinationRepo Inventory
+
+        # Forks the SYSINV project's SystemsInventory repository into the project with key RND
+        # Changes the 'SystemInventory' repository name to 'Inventory'
+
+    .FUNCTIONALITY
+        Stash
+    #>
+    [cmdletbinding()]
+    param(    
+        [parameter( Mandatory = $True )]
+        [string]$Project,
+        [parameter( Mandatory = $True )]
+        [string]$Repo,
+        [string]$DestinationProject,
+        [string]$DestinationRepo = $null,
+        [string]$Uri = $Script:StashConfig.Uri,
+        [System.Management.Automation.PSCredential]$Credential
+    )
+
+    #Build up URI
+        $StashBaseURI = Join-Parts -Separator "/" -Parts $Uri, "/rest/api/1.0"
+        $BaseUri = Join-Parts -Separator "/" -Parts $StashBaseURI, "/projects/$Project/repos/$Repo"
+
+#New-StashObject -body @{name=$null}
+
+    #Build up Invoke-RestMethod and Get-StashData parameters for splatting
+        $Body = @{
+            name = $DestinationRepo
+        }
+        if($PSBoundParameters.ContainsKey('DestinationProject'))
+        {
+            $Body.Add('project', @{key=$DestinationProject})
+        }
+
+        $IRMParams = @{
+            ErrorAction = 'Stop'
+            Uri = $BaseUri
+            Method = 'Post'
+            Body = $($Body | ConvertTo-JSON)
+            ContentType = 'application/json'
+        }
+        if($PSBoundParameters.ContainsKey('Credential'))
+        {
+            $IRMParams.Add( 'Headers', @{ Authorization = (Get-StashAuthString -Credential $Credential) } )
+        }
+
+        $GSDParams = @{ IRMParams = $IRMParams }
+
+    Write-Debug ( "Running $($MyInvocation.MyCommand).`n" +
+                    "PSBoundParameters:$( $PSBoundParameters | Format-List | Out-String)" +
+                    "Invoke-RestMethod parameters:`n$($IRMParams | Format-List | Out-String)" +
+                    "Get-StashData parameters:`n$($GSDParams | Format-List | Out-String)" )
+
+    Try
+    {
+        #Get the data from Stash
+        Get-StashData @GSDParams
+    }
+    Catch
+    {
+        Throw $_
+    }
+
+    Remove-Variable IRMParams -force -ErrorAction SilentlyContinue
+}
+
